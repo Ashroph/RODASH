bf8983c62f6fa52b4632d8e3b6451690f33cb3a2
Ashroph <akanniashraf02@gmail.com>
2023-04-14 01:26:12 +0100

    assignment1

---
+++ day1/personal.py
@@ -0,0 +1,36 @@
+#>>>>string
+# a = (1, 2, 3, 5)
+# b = (2, 3, 6, 7)
+# c = a + b
+# print ("the sum of a and b is", c)
+
+# >>>>numbers
+# x = 20
+# y = 15
+# z = x + y
+# print ("the sum of x and y is", z)
+
+# >>>>list
+# l = ["a", "b", "c", "d", "e"]
+# l0 = [1, 2, 3, 4, 5]
+# la = l[1]
+# lb = l[2]
+# lc = l[3]
+# l1 = l0[0]
+# l2 = l0[1]
+# l3 = l0[2]
+# print ( l1, l2, l3)
+# print (la, lb, lc)
+# print (len(l) )
+# print (l1 + l2)
+# l0.append(7)
+# print (l0)
+
+#>>>>tuple
+# t = ("ade", "bola", "seyi","ade", "yinka")
+# print(t.index("bola"))
+# print(t.count("ade"))
+# print(t[2])
+# print(t[2:])
+
+#>>>> set
