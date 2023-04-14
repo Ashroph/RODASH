c42d6efa603f410520d6c82e571054b27934f3f9
Ashroph <akanniashraf02@gmail.com>
2023-04-14 01:32:29 +0100

    ashroph

---
+++ day1/beginner.py
@@ -0,0 +1,33 @@
+# # call1 = "211"
+
+# # call2 = 'hi ashroph'
+
+# # call3 = call1 + call2
+
+# # print (call1, call2, call3)
+
+# if (1>0) :
+#     print ("yes, 1 is greater")
+# else :
+#     print ("no, 0 is lesser")    
+
+# ashroph = 'a'
+# ade = 'b'
+# if (ashroph == ade) :
+#     print ('yes')
+# else :
+#     print ('no')
+    
+
+A = input ("is there light?")
+if ( A == "yes") :
+        B = input("is the tutor around?")
+        if ( B == "yes") :
+                print ("ok, there will be class")
+elif ( A == "no") :
+       B = input("is the tutor around?")
+if ( B == "yes") :
+                print ("ok")
+else :
+    print ("tutor not coming")
+    
